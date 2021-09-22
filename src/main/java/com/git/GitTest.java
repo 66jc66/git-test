b1494f73ce0576264f11236b6368f985127a60eb
hw_glo <hehe@163.com>
2021-09-22 20:09:50 +0800

    other-fix commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello 欢迎你来到git");
         System.out.println("git是一个很强大的工具");
         System.out.println("在行业当中很受欢迎");
+        System.out.println("现在在另一个分支上进行操作");
     }
 }
