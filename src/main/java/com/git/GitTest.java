97a6fdf1d9c87980345e5c7c154da02e12cf6071
hw_glo <hehe@163.com>
2021-09-23 09:33:35 +0800

    fourth commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git是一个很强大的工具");
         System.out.println("在行业当中很受欢迎");
         System.out.println("现在在另一个分支上进行操作");
+        System.out.println("现在在原来开始分支上进行修改操作");
     }
 }
